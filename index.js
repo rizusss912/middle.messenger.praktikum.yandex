7607a58d5ae6668b5105e9ce98873cb712645ed8
Вадим Кошечкин <rizus@yandex-team.ru>
2021-07-02 19:00:21 +0300

    Create test route

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 app.use(express.static(path.join(__dirname + '/dist')));
+
+app.get('/test', function (req, res) {
+  res.status(200).send("test completed");
+})
 
 app.listen(PORT, () => {
   console.log(`Server has been started on http://localhost:${PORT}/`);
