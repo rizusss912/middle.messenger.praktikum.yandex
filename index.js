6f95fb2a166f7ebaef8d497f7355a8779c43d9d8
Вадим Кошечкин <rizus@yandex-team.ru>
2021-07-02 19:26:25 +0300

    Refactor server for netlify

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const serverless = require('serverless-http');
 const express = require('express');
 const path = require('path');
+
 const app = express();
+const router = express.Router();
+
 const PORT = 3002;
 
 app.use(express.static(path.join(__dirname + '/dist')));
@@ -14,4 +17,7 @@ app.get('/test', function (req, res) {
   console.log(`Server has been started on http://localhost:${PORT}/`);
 });*/
 
+app.use('/.netlify/functions/server', router)
+
+module.exports = app;
 module.exports.handler = serverless(app);
