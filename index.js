19bafd19ed2eb17c3fc016bb02b6058306884da0
Вадим Кошечкин <rizus@yandex-team.ru>
2021-07-07 22:01:25 +0300

    Fix server port

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
-const serverless = require('serverless-http');
+//const serverless = require('serverless-http');
 const express = require('express');
 const path = require('path');
 
 const app = express();
-const router = express.Router();
+//const router = express.Router();
 
-const PORT = 4004;
+const PORT = 3000;
 
 app.use(express.static(path.join(__dirname + '/dist')));
-
-app.get('/test', function (req, res) {
-  res.status(200).send("test completed");
-})
 
 app.listen(PORT, () => {
   console.log(`Server has been started on http://localhost:${PORT}/`);
 });
 
-app.use('/.netlify/functions/server', router)
+//app.use('/.netlify/functions/server', router)
 
 //module.exports = app;
 //module.exports.handler = serverless(app);
